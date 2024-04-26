1304e3882ec98db9d519faebea71ffa7a2e967ea
flakhal <162927105+flakhal@users.noreply.github.com>
2024-04-26 13:43:34 +0200

    Update Formuls.jsx

--- OeuvreSociale/front/src/Components/Formuls.jsx
+++ OeuvreSociale/front/src/Components/Formuls.jsx
@@ -17,7 +17,7 @@ return(
 
 </Link>    
 
-   <Link  to="/formulaire/formulairepret" className="linkwrapper" >
+   <Link  to="/formulaire/ajouteroffre" className="linkwrapper" >
     
     
         <div className="linktodem"> Ajouter formulaire de prêt</div>
